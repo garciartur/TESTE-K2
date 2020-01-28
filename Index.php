019bc58a52f1bc0bbe2beca0a4779b7263864694
Artur Garcia <arturfgarcia@gmail.com>
2020-01-28 14:42:52 -0300

    Criando arquivo base com formulário em HTML

---
+++ Index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+	<head>
+		<meta charset="UTF-8">
+		<title>Upload multimídia</title>
+		<link rel="stylesheet" href="style.css"
+	</head>
+
+	<body>
+		<form method="post" action="" enctype="multipart/form-data">
+		<input type="file" name="arquivo" required>
+		<input type="submit">
+		</form>
+	</body>
+
+</html>
---
+++ README.md
@@ -0,0 +1,4 @@
+# TESTE K2
+
+Formulário de upload de imagens e vídeos para teste de habilidades em PHP, HTML e CSS.
+O sistema recebe o arquivo e sua descrição e ao recarregar mostra as informações upadas.
