014df0b8b9896df2f605ce9718cf38c5fe7755f8
Artur Garcia <arturfgarcia@gmail.com>
2020-01-29 12:57:41 -0300

    Finalizando sistema de upload com descrição em php

--- Index.php
+++ Index.php
@@ -9,20 +9,30 @@
 
 	<body>
 		<form method="post" action="" enctype="multipart/form-data">
-		<input type="file" name="arquivo" required>
-		<input type="submit">
+		<p class="formulario"><input type="file" name="arquivo" required></p>
+		<p class="formulario">Descrição: <input type="text" name="descricao" required></p>
+		<p class="formulario"><input type="submit"></p>
 		</form>
 
 	<?php
 		$arquivo=isset($_FILES['arquivo'])?$_FILES['arquivo']:"";
+		$arquivo=isset($_GET['arquivo'])?$_FILES['arquivo']:"";
 
 		if (isset($_FILES['arquivo'])){
 			$nome = $arquivo ['name'];
-			$extperm = ['jpg','jpeg','png'];
-			$tamanho = $arquivo['size'];
+			$extperm = ['jpg','jpeg','png', 'mp4'];
 			$extensao = explode ('.' , $nome);
 			$extensao = end($extensao);
-			$renomear = mD5(time());
+			$rename = mD5(time()) . '.' .$extensao;
+
+			if (in_array($extensao, $extperm)) {
+				$upload = move_uploaded_file($_FILES['arquivo']['tmp_name'], 'uploads/' .$rename);
+				echo "Parabéns! Seu upload foi efetuado com sucesso.";
+				print "<img src='uploads/$rename' width='150'>";
+			}
+			else{
+				echo "Favor enviar apenas arquivos de vídeo ou imagem.";
+			}
 		}
 	?>
 	</body>
