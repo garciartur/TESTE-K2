c249b9e5d01aa077edde9103a949f130c2c98856
Artur Garcia <arturfgarcia@gmail.com>
2020-01-30 11:16:10 -0300

    Adicionando stylesheet

--- Index.php
+++ Index.php
@@ -4,10 +4,16 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Upload multimídia</title>
-		<link rel="stylesheet" href="style.css"
+		<link rel="stylesheet" href="style.css">
 	</head>
 
 	<body>
+		<header>
+			<div class="caixa">
+				<h1>Upload multimídia</h1>
+			</div>
+		</header>
+
 		<form method="post" action="" enctype="multipart/form-data">
 		<p class="formulario"><input type="file" name="arquivo" required></p>
 		<p class="formulario">Descrição: <input type="text" name="descricao" required></p>
@@ -16,11 +22,10 @@
 
 	<?php
 		$arquivo=isset($_FILES['arquivo'])?$_FILES['arquivo']:"";
-		$arquivo=isset($_GET['arquivo'])?$_FILES['arquivo']:"";
 
 		if (isset($_FILES['arquivo'])){
 			$nome = $arquivo ['name'];
-			$extperm = ['jpg','jpeg','png', 'mp4'];
+			$extperm = ['jpg','jpeg','png', 'bmp', 'tiff', 'gif','mp4', 'avi','flv','mov'];
 			$extensao = explode ('.' , $nome);
 			$extensao = end($extensao);
 			$rename = mD5(time()) . '.' .$extensao;
@@ -28,7 +33,8 @@
 			if (in_array($extensao, $extperm)) {
 				$upload = move_uploaded_file($_FILES['arquivo']['tmp_name'], 'uploads/' .$rename);
 				echo "Parabéns! Seu upload foi efetuado com sucesso.";
-				print "<img src='uploads/$rename' width='150'>";
+				echo "<div><img src='uploads/$rename' width='150'></div>";
+				echo $_POST ['descricao'];
 			}
 			else{
 				echo "Favor enviar apenas arquivos de vídeo ou imagem.";
